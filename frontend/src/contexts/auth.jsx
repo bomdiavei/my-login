a7a94160b08b6987768432a8be88675a5f9e2066
bomdiavei <bomdiavei@hotmail.com>
2022-02-16 15:23:28 -0400

    Local Storage

--- frontend/src/contexts/auth.jsx
+++ frontend/src/contexts/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 
 import { useNavigate } from 'react-router-dom'
 
@@ -7,24 +7,43 @@ export const AuthContext = createContext()
 export const AuthProvider = ({ children }) => {
     const navigate = useNavigate()
     const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const recoveredUser = localStorage.getItem("user")
+
+        if (recoveredUser) {
+            setUser(JSON.parse(recoveredUser))
+        }
+
+        setLoading(false)
+    }, [])
 
     const login = (email, password) => {
         console.log(`login ${email} ${password}`,)
 
+        const loggedUser = {
+            id: "123",
+            email
+        }
+
+        localStorage.setItem("user", JSON.stringify(loggedUser))
+
         if (password === "test") {
-            setUser({ id: "123", email })
+            setUser(loggedUser)
             navigate("/")
         }
     }
 
     const logout = (email, password) => {
         console.log("logout")
+        localStorage.removeItem("user")
         setUser(null)
         navigate("/login")
     }
 
     return (
-        <AuthContext.Provider value={{ authenticated: !!user, user, login, logout }}>
+        <AuthContext.Provider value={{ authenticated: !!user, user, loading, login, logout }}>
             {children}
         </AuthContext.Provider>
     )
